7710480cfb10b0373a40aa4d8df94d398d7eed26
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-17 16:43:51 +0100

    pineapple controller bitches!

--- src/main/java/com/cegekaschool/controllers/PineappleController.java
+++ src/main/java/com/cegekaschool/controllers/PineappleController.java
@@ -1,9 +1,11 @@
 package com.cegekaschool.controllers;
+
 import com.cegekaschool.domain.pineapple.Pineapple;
 import com.cegekaschool.domain.pineapple.PineappleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
@@ -11,12 +13,22 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/pineapple")
-public class  PineappleController {
+public class PineappleController {
     @Inject
     private PineappleService pineappleService = new PineappleService();
 
-    @RequestMapping (method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public List<Pineapple> getPineapples
+    List<Pineapple> getPineapples() {
+        return pineappleService.getAllPineapples();
+    }
 
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    void addPineapple(
+            @RequestParam(value = "firstName", required = true) String firstName,
+            @RequestParam(value = "lastName", required = true) String lastName) {
+        pineappleService.addPineapple(firstName, lastName);
+    }
 }
+
