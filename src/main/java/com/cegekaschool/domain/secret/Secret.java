f7ec6554b3585cc793dd4a1d034f2102451b947a
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-17 12:55:07 +0100

    Implemented secret

--- src/main/java/com/cegekaschool/domain/secret/Secret.java
+++ src/main/java/com/cegekaschool/domain/secret/Secret.java
@@ -1,7 +1,25 @@
 package com.cegekaschool.domain.secret;
 
+import com.cegekaschool.domain.photo.Photo;
+import com.cegekaschool.domain.pineapple.Pineapple;
+
 /**
  * Created by roelg on 17/02/2017.
  */
 public class Secret {
+    private Pineapple pineapple;
+    private Photo photo;
+
+    public Secret(Pineapple pineapple, Photo photo) {
+        this.pineapple = pineapple;
+        this.photo = photo;
+    }
+
+    public Pineapple getPineapple() {
+        return pineapple;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
 }
