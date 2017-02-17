a42b812ebfe106b180d26748e0f73003eda75540
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-17 16:37:33 +0100

    Implemented controller

--- src/main/java/com/cegekaschool/controllers/SecretController.java
+++ src/main/java/com/cegekaschool/controllers/SecretController.java
@@ -1,6 +1,10 @@
 package com.cegekaschool.controllers;
 
+import com.cegekaschool.domain.secret.Secret;
 import com.cegekaschool.domain.secret.SecretService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -9,8 +13,16 @@ import javax.inject.Named;
  * Created by roelg on 17/02/2017.
  */
 @Named
+@Controller
+@RequestMapping("/secret")
 public class SecretController {
     @Inject
     private SecretService secretService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createSecret(Secret secret) {
+        secretService.addSecret(secret);
+    }
+
 
 }
