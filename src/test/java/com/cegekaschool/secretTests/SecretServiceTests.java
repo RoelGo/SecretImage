4c78c6bd89e910cee509acb89dc3a51613a9bfad
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-17 12:57:10 +0100

    Test for test for secret service

---
+++ src/test/java/com/cegekaschool/secretTests/SecretServiceTests.java
@@ -0,0 +1,44 @@
+package com.cegekaschool.secretTests;
+
+import com.cegekaschool.domain.secret.Secret;
+import com.cegekaschool.domain.secret.SecretRepository;
+import com.cegekaschool.domain.secret.SecretService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by roelg on 17/02/2017.
+ */
+public class SecretServiceTests {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @InjectMocks
+    private SecretService testSecretService;
+    @Mock
+    private SecretRepository secretRepository;
+    @Mock
+    private Secret testSecret1;
+
+    @Before
+    public void setUp() throws Exception {
+        testSecretService=new SecretService();
+
+
+    }
+
+    @Test
+    public void CheckIf_SecretService_Calls_SecretRepositryAddMethod(){
+        testSecretService.addSecret(testSecret1);
+        verify(secretRepository.addSecret(testSecret1));
+
+    }
+}
