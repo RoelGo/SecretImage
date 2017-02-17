6d9f56ab69a73a151bb18ec77e5deb3dda9591e9
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-17 12:48:02 +0100

    Added depency for web

--- src/main/java/com/cegekaschool/controllers/PineappleController.java
+++ src/main/java/com/cegekaschool/controllers/PineappleController.java
@@ -1,7 +1,13 @@
 package com.cegekaschool.controllers;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by roelg on 17/02/2017.
- */
-public class PineappleController {
+
+@Controller
+@RequestMapping("/pineapple")
+public class  PineappleController {
+    @
+    private PineappleController pineappleController = new PineappleController();
+
 }
