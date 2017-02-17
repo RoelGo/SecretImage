8bc5a661aa6fcf4567ecbda9a1d15c4f1ca1eb51
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-17 10:09:33 +0100

    First commit
    Made project  with spring initializr

---
+++ src/main/java/com/cegekaschool/SecretImageApplication.java
@@ -0,0 +1,12 @@
+package com.cegekaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretImageApplication.class, args);
+	}
+}
