a24e381fc16cfa86f09eb51b1120468dc7537e06
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-20 09:52:42 +0100

    The pineapples will be added as well

--- src/main/java/com/cegekaschool/controllers/SecretController.java
+++ src/main/java/com/cegekaschool/controllers/SecretController.java
@@ -2,6 +2,7 @@ package com.cegekaschool.controllers;
 
 import com.cegekaschool.domain.photo.Photo;
 import com.cegekaschool.domain.pineapple.Pineapple;
+import com.cegekaschool.domain.pineapple.PineappleService;
 import com.cegekaschool.domain.secret.Secret;
 import com.cegekaschool.domain.secret.SecretService;
 import org.springframework.stereotype.Controller;
@@ -26,11 +27,14 @@ import java.util.StringJoiner;
 public class SecretController {
     @Inject
     private SecretService secretService;
+    @Inject
+    private PineappleService pineappleService;
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     public void createSecret(@RequestBody Secret secret) {
         secretService.addSecret(secret);
+        pineappleService.addPineapple(secret.getPineapple().getFirstName(),secret.getPineapple().getLastName());
     }
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
