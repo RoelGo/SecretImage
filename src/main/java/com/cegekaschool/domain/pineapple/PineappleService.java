7bda54573d637f1ead34f9591e38fec1a5216bee
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-22 14:07:22 +0100

    Validation for unique pineaples

--- src/main/java/com/cegekaschool/domain/pineapple/PineappleService.java
+++ src/main/java/com/cegekaschool/domain/pineapple/PineappleService.java
@@ -6,18 +6,24 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Named
 public class PineappleService {
+    private final AtomicLong counter = new AtomicLong();
     @Inject
     PineappleDatatbaseRepository pineappleRepository;
-
-    private final AtomicLong counter = new AtomicLong();
 
     public Iterable<Pineapple> getAllPineapples() {
         return pineappleRepository.findAll();
     }
 
     public Pineapple addPineapple(String firstName, String lastName) {
-        Pineapple output = new Pineapple(counter.incrementAndGet(), firstName, lastName);
+        Pineapple output = findPineapple(firstName, lastName);
+        if (output == null) {
+            output = new Pineapple(counter.incrementAndGet(), firstName, lastName);
+        }
         pineappleRepository.save(output);
         return output;
+    }
+
+    private Pineapple findPineapple(String firstName, String lastName) {
+        return pineappleRepository.findByFirstNameAndLastName(firstName, lastName);
     }
 }
