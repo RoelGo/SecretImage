e471d9df20e3a7671d784746d7474da5b73666c2
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-22 11:35:47 +0100

    Created a mapper

---
+++ src/main/java/com/cegekaschool/domain/Mapper.java
@@ -0,0 +1,40 @@
+package com.cegekaschool.domain;
+
+import com.cegekaschool.domain.photo.Photo;
+import com.cegekaschool.domain.photo.PhotoDTO;
+import com.cegekaschool.domain.photo.PhotoService;
+import com.cegekaschool.domain.pineapple.Pineapple;
+import com.cegekaschool.domain.pineapple.PineappleDTO;
+import com.cegekaschool.domain.pineapple.PineappleService;
+import com.cegekaschool.domain.secret.Secret;
+import com.cegekaschool.domain.secret.SecretDTO;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Created by roelg on 22/02/2017.
+ */
+@Named
+public class Mapper {
+
+    @Inject
+    private PineappleService pineappleService;
+
+    @Inject
+    private PhotoService photoService;
+
+
+    public Secret mapSecret(Long id, SecretDTO secretDTO) {
+        return new Secret(id, mapPineapple(secretDTO.getPineapple()), mapPhoto(secretDTO.getPhoto()));
+    }
+
+    private Photo mapPhoto(PhotoDTO photo) {
+        return photoService.addPhoto(photo.getPhotoLocation());
+    }
+
+    private Pineapple mapPineapple(PineappleDTO pineappleDTO) {
+        return pineappleService.addPineapple(pineappleDTO.getFirstName(), pineappleDTO.getLastName());
+    }
+
+}
